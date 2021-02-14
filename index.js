d9c11b0c7200a488117b7aff8e7ea9b8aecce396
goodgoodcoder <jongyunj@icloud.com>
2021-02-14 18:34:14 +0900

    Express Server, ES6 using babel

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hi");
+import "core-js";
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
+
+const handleHome = (req, res) => {
+  res.send("Hello World");
+};
+
+// app.get("/", function (req, res) {
+//   res.send("Hello World");
+// });
+
+app.get("/", handleHome);
+
+app.listen(PORT, handleListening);
